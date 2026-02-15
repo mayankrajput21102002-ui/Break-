f4f165ed97b6f758e3481924d70b4692103dfa97
mayankrajput21102002-ui <mayankrajput21102002@gmail.com>
2026-02-15 22:00:29 +0530

    Create database configuration file

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+// Database configuration
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql' // or 'postgres', 'sqlite', 'mssql'
+});
+
+module.exports = sequelize;
